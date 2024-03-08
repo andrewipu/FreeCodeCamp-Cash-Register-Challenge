4b76210bacf39732bf1efe257b898daf455539a4
Andrew Ipu <ipuandrew@gmail.com>
2024-03-08 14:31:10 +0300

    Added strict mode

--- cash register.js
+++ cash register.js
@@ -1,6 +1,27 @@
+'use strict';
+
 function checkCashRegister(price, cash, cid) {
-    let change;
-    return change;
+    //calculate change
+    let change = cash - price;
+    console.log(change);
+
+    //get total cash in register
+    let totalCID = 0;
+
+    
+
+
+
+
+    //?check if total cash-in-drawer == to change.
+    //if so, return {status: "CLOSED", change: cid}
+    //This means I don't need to give any change,
+    //and the register can be CLOSED.
+    //?
+
+    
+    
   }
   
-  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  //console.log(checkCashRegister);
